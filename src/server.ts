61fa3b2376ddb2b36c5f58f990bfe1f7a183d1aa
khristopher <khristopheroficial@gmail.com>
2024-05-09 18:12:23 -0400

    sistema pizzaria

--- src/server.ts
+++ src/server.ts
@@ -34,5 +34,5 @@ app.listen({
     host: '0.0.0.0',
     port: process.env.PORT ? Number(process.env.PORT) : 3333,
 }, () => {
-    console.log('Servidor online');
+    console.log('HTTP Server Running');
 });
